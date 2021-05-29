b01fa6d0de8476643a2f72da2275447ff32492e4
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-05-29 10:22:06 -0700

    first commit

---
+++ desktop_sync.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from telegram_util import log_on_fail
+import album_sender
+import threading
+import plain_db
+from telegram.ext import Updater
+import os
+
+with open('token') as f:
+	token = f.read().strip()
+tele = Updater(token, use_context=True)  # @weibo_subscription_bot
+debug_group = tele.bot.get_chat(420074357)
+
+existing = plain_db.loadKeyOnlyDB('existing')
+
+@log_on_fail(debug_group)
+def loopImp():
+	
+	
+		
+def loop():
+	loopImp()
+	threading.Timer(10, loop).start() 
+
+if __name__ == '__main__':
+	threading.Timer(1, loop).start() 
+	tele.start_polling()
+	tele.idle()
